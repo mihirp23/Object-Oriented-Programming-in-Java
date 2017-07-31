613246f6c8dd49f84acfb85533e64d181c134148
mihirp23 <mihirp38661@gmail.com>
2017-07-31 12:21:42 -0400

    Implement battle logic

--- Battleship.java
+++ Battleship.java
@@ -64,13 +64,102 @@ public class Battleship {
 			num_of_ships++;
 		}
 	}
+	/*
+	 * ------------------------------- battle() -------------------------------
+	 */
+	public void battle() {
+		
+		System.out.println("Battle!!!");
+		
+		int x, y;
+		Scanner in = new Scanner(System.in);
+		boolean isPlayerTurn = true;
+		
+		int computerShips = 5, userShips = 5;
+		
+		do {
+			
+			this.displayOcean();
+			
+			if (isPlayerTurn) {
+				System.out.println("YOUR TURN!");
+			    System.out.println("enter X coordinate of the attack position ");
+			    x = in.nextInt();
+			    System.out.println("enter Y coordinate of the attack position ");
+			    y = in.nextInt();
+			}
+			else {
+				System.out.println("COMPUTER's TURN!");
+				x = (int) (Math.random() * 10);
+				y = (int) (Math.random() * 10); 
+			}
+			   
+			// invalid coordinates
+			if (x < 0 || x > 9 || y < 0 || y > 9) {
+			   continue;
+			}
+			    
+			// attacked a position that was previously attacked.
+			if (ocean[x][y] == 'x') {
+			    continue;
+			}
+			
+			
+			// interact with user on how the attack happened.
+			if (ocean[x][y] == '#'){
+				if (isPlayerTurn) {
+					System.out.println("Boom, your sunk a computer ship!");
+				}
+				else {
+					System.out.println("Computer sunk its own ship!");
+				}
+			    computerShips--;
+			}
+			else if (ocean[x][y] == '@') {
+				if (isPlayerTurn) {
+					System.out.println("You sunk your own ship! :( ");
+				}
+				else {
+					System.out.println("Computer sunk your ship!");
+				}
+			    userShips--;
+			}
+			else {
+				System.out.println("Missed!!!");
+			}
+			
+			ocean[x][y] = 'x';
+			
+			// switch between player and computer
+			if (isPlayerTurn) {
+				isPlayerTurn = false;
+			}
+			else {
+				isPlayerTurn = true;
+			}
+			
+		} while (userShips > 0 && computerShips > 0);
+		
+		System.out.println();
+		System.out.println("============================================");
+		if (userShips == 0) {
+			System.out.println("Computer has won!");
+		}
+		else {
+			System.out.println("User has won!");
+		}
+		System.out.println("============================================");
+		
+		
+	}
 
 	/*
 	 * ---------------------------- placeComputerShips() -----------------------
 	 */
 	public void placeComputerShips() {
 		int num_of_ships = 0;
-
+        
+		System.out.println("Computer is deploying ships!");
 		while (num_of_ships < 5) {
 			int x = (int) (Math.random() * 10);
 			int y = (int) (Math.random() * 10);
@@ -78,18 +167,20 @@ public class Battleship {
 			if (ocean[x][y] != '*') {
 				continue;
 			}
-
-			ocean[x][y] = 'x';
+            
+			System.out.println("Ship deployed...");
+			ocean[x][y] = '#';
 			num_of_ships++;
 		}
+		
+		System.out.println("-----------------------------------------------");
 	}
 
 	public static void main(String[] args) {
 		Battleship b = new Battleship();
 		b.displayOcean();
 		b.placeUserShips();
-		b.displayOcean();
 		b.placeComputerShips();
-		b.displayOcean();
+		b.battle();
 	}
 }
